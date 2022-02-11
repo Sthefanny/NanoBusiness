d41e8562a810a9f65d8a723ca6fd9247f02251c2
Ana C <carolacsc@hotmail.com>
2022-02-11 15:32:00 -0300

    Home e inicio do settings

---
+++ cyberunner/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  cyberunner
+//
+//  Created by Ana C S Costa on 10/02/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
